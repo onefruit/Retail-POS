9c2227cac86198963481da3dd0ff60ed4c3a3d9c
Prabin-sc1 <prabin.sc1@gmail.com>
2025-11-22 02:19:13 +0545

    feat: Category fetching and adding

--- retail-frontend/src/components/CategoryForm/CategoryForm.jsx
+++ retail-frontend/src/components/CategoryForm/CategoryForm.jsx
@@ -1,14 +1,76 @@
-import React from "react";
+import { useContext, useEffect, useState } from "react";
+import { assets } from '../../assets/assets';
+import { addCategory } from "../../service/CategoryService";
+import { AppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
+
 
 const CategoryForm = () => {
+const {categories, setCategories} = useContext(AppContext);
+
+  const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState(false);
+  const [data, setData] = useState({
+    name: "",
+    description: "",
+    bgColor: "#2c2c2c",
+  });
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  const onChangeHandler = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+
+  const onSubmitHandler = async (e)=>{
+      e.preventDefault();
+      if (!image){
+        toast.error("please select image");
+        return;
+      }
+      setLoading(true);
+
+      const formData = new FormData();
+      formData.append('category', JSON.stringify(data));
+      formData.append("file", image);
+      try{
+        const response = await addCategory(formData);
+        if (response.status === 201){
+            setCategories([...categories,response.data]);
+            toast.success("Category added");
+            setData({
+              name: "",
+              description: "",
+              bgColor:"#2c2c2c"
+            });
+            setImage(false);
+
+        }
+      }catch(err){
+        console.error(err);
+        toast.error("error adding category")
+    }finally{
+      setLoading(false);
+    }
+  }
+
   return (
     <div className="mx-2 mt-2">
-      <div className="card col-md-8 form-container">
+      <div className="card col-md-12 form-container">
         <div className="card-body">
-          <form>
+          <form onSubmit={onSubmitHandler}>
             <div className="mb-3">
               <label htmlFor="image" className="form-label">
-                <img src="https://placehold.co/48x48" alt="" width={48} />
+                <img
+                  src={image ? URL.createObjectURL(image) : assets.upload}
+                  alt=""
+                  width={48}
+                />
               </label>
               <input
                 type="file"
@@ -16,6 +78,7 @@ const CategoryForm = () => {
                 id="image"
                 className="form-control"
                 hidden
+                onChange={(e) => setImage(e.target.files[0])}
               />
             </div>
 
@@ -29,11 +92,11 @@ const CategoryForm = () => {
                 id="name"
                 placeholder="Category Name"
                 className="form-control"
+                onChange={onChangeHandler}
+                value={data.name}
               />
             </div>
 
-
-            
             <div className="mb-3">
               <label htmlFor="name" className="form-label">
                 Description
@@ -44,19 +107,30 @@ const CategoryForm = () => {
                 id="description"
                 placeholder="Write content here..."
                 className="form-control"
+                onChange={onChangeHandler}
+                value={data.description}
               />
             </div>
-            
+
             <div className="mb-3">
-                <label htmlFor="bgcolor" className="form-label">Background Color</label>
-                <br />
-                <input type="color" name="bgColor"
+              <label htmlFor="bgcolor" className="form-label">
+                Background Color
+              </label>
+              <br />
+              <input
+                type="color"
+                name="bgColor"
                 id="bgColor"
-                placeholder="#ffffff" />
+                onChange={onChangeHandler}
+                value={data.bgColor}
+              />
             </div>
 
-            <button type="submit" className="btn btn-primary w-100">Save</button>
-
+            <button type="submit" 
+            disabled={loading}
+            className="btn btn-primary w-100">
+              {loading ? "Loading...":"Submit"}
+            </button>
           </form>
         </div>
       </div>
